29ffc1d80ecf8dfa0088832403e3fdcbea95cfd4
YugMovaliya2122 <movaliyayug950@gmail.com>
2025-07-05 17:12:26 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -27,7 +27,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppProvider } from './component/context/AppContext.jsx'
+import { AppProvider } from './components/context/AppContext.jsx'
 //this is for routing 
 //like js 
 //to shift one page to another
