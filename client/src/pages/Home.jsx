8f3b2a6781785e8854069118c9b7cc9d4d9d9418
YugMovaliya2122 <movaliyayug950@gmail.com>
2025-07-05 18:17:47 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
 import Newsletter from '../components/Newsletter'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <><Navbar/>
